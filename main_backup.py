92ecc05406fd7690708d7bc946534f6fef29bad9
Smart2Eco <nutsinaporn@yahoo.com>
2025-06-26 15:26:14 +0700

    Create main_backup.py

---
+++ main_backup.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from ESP32 LINE Flask server!"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.get_json()
+    print("✅ ได้รับข้อมูลจาก LINE:", data)
+    return '', 200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
